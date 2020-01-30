be2093224896e253b5b88410d03af327967bed6f
Martijn <martijnvermeulen1@gmail.com>
2020-01-30 17:23:49 +0100

    registration_form completed 30_01_2020_16_36

--- webshop/users/views.py
+++ webshop/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
 from .forms import UserRegistrationForm
 
 def register(request):
@@ -11,7 +12,14 @@ def register(request):
     else:
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('main_shop/shop_home'));
+            new_user = form.save()
+            authenticated_user = authenticate(username = new_user.username,
+                    password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('shop_home'));
 
     return render(request, 'users/register.html', {'form' : form})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('shop_home'))
